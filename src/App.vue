972d635c4c3b886a36064ab0bb050e4545fa8b46
zesciati <zakyabiyyu2003@gmail.com>
2026-06-29 16:24:31 +0700

    first initialize

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <div class="flex justify-center relative">
+      <div class="relative top-12">
+        <img
+          src="../images/image-tanya.jpg"
+          alt=""
+          class="w-50 rounded-md"
+        />
+        <div class="flex items-center absolute justify-center left-15 space-x-10 bg-white px-2 py-2 rounded-2xl bottom-[-16px]">
+          <img src="../images/icon-prev.svg" alt="" />
+          <img src="../images/icon-next.svg" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
