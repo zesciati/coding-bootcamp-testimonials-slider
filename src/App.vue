2d7dd494a896b30e1fbe048f69928710aa3d4e01
zesciati <zakyabiyyu2003@gmail.com>
2026-07-04 20:19:25 +0700

    desktop design

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="md:grid md:grid-cols-2 ">
+  <div class="md:grid md:grid-cols-2 md:mx-auto md:w-190 ">
     <section class="flex justify-center md:col-start-2 md:place-self-start ">
       <div class="relative top-12 ">
         <img
@@ -26,13 +26,13 @@
         <img
           src="../images/pattern-bg.svg"
           alt="pattern background"
-          class="w-75 absolute -top-4 -left-9 -z-10 max-w-none md:w-110"
+          class="w-75 absolute -top-4 -left-9 -z-10 max-w-none md:w-103"
         />
       </div>
     </section>
 
-    <section class="mt-35 space-y-4 text-lg text-center md:text-left md:col-start-1 md:row-start-1 ">
-      <div class="px-10 z-20 relative md:px-0 ">
+    <section class="mt-35 space-y-4 text-lg text-center md:text-left md:col-span-1 md:row-start-1 md:relative md:z-20 md:left-16">
+      <div class="px-10 z-20 relative md:px-0">
         <span class="">
           "I've been interested in coding for a while but never taken the jump,
           until now. I couldn't recommend this course enough. I'm now in the job
@@ -45,7 +45,7 @@
           class="z-10 absolute w-15 -top-5.5 left-39"
         />
       </div>
-      <div class="md:flex gap-x-2">
+      <div class="md:flex md:gap-x-2  ">
         <div class="font-bold">Tanya Sinclair</div>
         <div>UX Engineer</div>
       </div>
