60445b02f258e8489ddd2f6c28ba3a93daa6df73
zesciati <zakyabiyyu2003@gmail.com>
2026-07-09 16:22:55 +0700

    changing cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ app.use(
     origin: [
       "https://coding-bootcamp-testimonials-slider.pages.dev",
       "http://localhost:5173",
-      "http://localhost:3000/api/testimonials",
     ],
   })
 ); // allows requests from any origin
