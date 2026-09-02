c105fc981f4f59ca4ea03ce198e0e9b4a440070a
zesciati <zakyabiyyu2003@gmail.com>
2026-09-02 10:11:21 +0700

    coba lagi

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,19 @@ import express, {
   type NextFunction,
 } from "express";
 import cors from "cors";
-import {Testimonials} from "./data/testimonials.json";
-import { error } from "console";
+import { Testimonials } from "./data/testimonials.json";
+import { httpServerHandler } from "cloudflare:node";
 
 const app = express();
-app.use(cors()); // allows requests from any origin
-app.use(express.json()); // can read json format data sent by client
-
-
+app.use(
+  cors({
+    origin: [
+      "https://coding-bootcamp-testimonials-slider.zakyabiyyu2003.workers.dev",
+      "http://localhost:5173",
+    ],
+  })
+);
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("halo gimana");
@@ -21,24 +26,11 @@ app.get("/api/testimonials", (req, res) => {
   res.json(Testimonials);
 });
 
-// app.get('/api/testimonials/:id', (req, res) => {
-//   const item = Testimonials.find(t => t.id === req.params.id);
-//   if(!item){
-//     return res.status(404).json({error: 'Testimonials tidak ditemukan'});
-//   }
-//   res.json(item);
-// })
-
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
-const PORT = process.env.PORT || 3000;
+app.listen(3000);
 
-app.listen(PORT, (error: any) => {
-  if (error) {
-    throw error;
-  }
-  console.log("server is running on port 3000");
-});
+export default httpServerHandler({ port: 3000 });
